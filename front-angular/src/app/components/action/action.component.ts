7da9e9f87d57d29dce4c4d0244eaaef1bb22257f
IwonaGorska <iwonagorskaa@gmail.com>
2020-12-09 00:02:49 +0100

    Work on left side

--- front-angular/src/app/components/action/action.component.ts
+++ front-angular/src/app/components/action/action.component.ts
@@ -12,4 +12,45 @@ export class ActionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  operations = [
+       {
+          "label": "Tworzenie pojedynczego rekordu",
+          "isInput": true,
+          "dropdown": [],
+          "dropdownTitle": ""
+       },
+       {
+          "label": "Tworzenie wielu rekordów",
+          "isInput": true,
+          "dropdown": [10, 100], 
+          "dropdownTitle": "Ile"
+       },
+       {
+         "label": "Zastąpienie wielu rekordów nowymi wartościami",
+         "isInput": true,
+         "dropdown": [10, 100], 
+         "dropdownTitle": "Ile"
+       },
+       {
+         "label": "Wyszukanie rekordu",
+         "isInput": false,
+         "dropdown": [],
+         "dropdownTitle": ""
+       },
+       {
+         "label": "Usunięcie pojedynczego rekordu",
+         "isInput": false,
+         "dropdown": [],
+         "dropdownTitle": ""
+       },
+       {
+         "label": "Usunięcie wielu rekordów",
+         "isInput": false,
+         "dropdown": [10, 100], /*ile */
+         "dropdownTitle": "Ile"
+       }
+    ];
+
+  
+
 }
