60a9f6e96362a19884e06b7f8aa3a7facb5ea959
IwonaGorska <iwonagorskaa@gmail.com>
2020-12-08 21:53:39 +0100

    New components for home page

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InfoComponent } from './components/info/info.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ActionComponent } from './components/action/action.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    HomepageComponent,
+    ActionComponent
   ],
   imports: [
     BrowserModule,
