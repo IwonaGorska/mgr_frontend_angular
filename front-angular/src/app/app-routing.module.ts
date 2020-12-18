3d164ba66078ff1e721d30e802f480592343c1f1
IwonaGorska <iwonagorskaa@gmail.com>
2020-12-18 23:38:18 +0100

    Routing, navigation with home and chat

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ChatComponent } from './components/chat/chat.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  }, // I observed that I should use either this or app-homepage selector in app.component.html
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-angular/src/app/components/header/header.component.ts
+++ front-angular/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute, ) { }
 
   ngOnInit(): void {
+    console.log("URL = " + JSON.stringify(this.route.url));
   }
 
 }
